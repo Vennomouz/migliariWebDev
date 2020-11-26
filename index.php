5c9f29907bd3116d01921c4173967e1d5a61275e
Julio Migliari <julio.augmi@hotmail.com>
2020-11-26 15:50:36 -0300

    dando um estilo

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Goldman:wght@700&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Goldman:wght@700&family=Indie+Flower&display=swap" rel="stylesheet"> 
     </head>
 
     <header>
@@ -16,6 +19,9 @@
                 <li>Sobre mim</li>
                 <li>Serviços</li>
             </ul>
+            <div class="tituloBanner">
+                <h1>Web Developer</h1>
+            </div>
         </div>
     </header>
 
