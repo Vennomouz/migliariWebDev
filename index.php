0478bd727c5889687c72cf644844e45548074ca0
Julio Migliari <julio.augmi@hotmail.com>
2020-11-26 13:04:42 -0300

    iniciando projeto

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Migliari Web Dev</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Migliari Web Dev</title>
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
 
-<body>
-</body>
+    <header>
+        <div class="menuPrincipal">
+            <ul>
+                <li>Home</li>
+                <li>Sobre mim</li>
+                <li>Serviços</li>
+            </ul>
+        </div>
+    </header>
+
+    <body>
+    </body>
 
 </html> 
