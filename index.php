80cbd75994e0a288c8797df869652e1c4afc87b2
Julio <julio.augmi@hotmail.com>
2020-11-30 21:00:19 -0300

    mudando tudo

--- index.php
+++ index.php
@@ -4,14 +4,19 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Migliari Web Dev</title>
-        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link href="fontawesome/css/all.css" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Vesper+Libre:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap" rel="stylesheet"> 
     </head>
 
     <header>
+        <div class="redes">
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-twitter"></i>
+        </div>
         <div class="menuPrincipal">
             <ul>
                 <li><a href="">Home</a></li>
